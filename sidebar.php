ea3b006d0a8c4f0cb5c422e57ea8501d4d450ff3
Shihab Uddin <74145122+Shihab-278@users.noreply.github.com>
2024-12-15 22:01:54 +0600

    Index and sidebar update

--- sidebar.php
+++ sidebar.php
@@ -15,91 +15,46 @@
     <!-- recent posts box -->
     <div class="recent-post-container">
         <h4>Recent Posts</h4>
+        <?php
+            include "admin/config.php";
+            $limit = 3;
+
+            $query = "SELECT post.post_id, post.title, post.post_img, post.post_date, post.category, category.category_name FROM post
+            LEFT JOIN category ON post.category = category.category_id
+            ORDER BY post.title ASC 
+            LIMIT {$limit}";
+
+            $result = mysqli_query($connection, $query) or die("Failed");
+            $count = mysqli_num_rows($result);
+
+            if ($count > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+
+                    
+        ?>
         <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post-format.jpg" alt=""/>
+            <a class="post-img" href="single.php?id=<?php echo $row['post_id'] ?>">
+                <img src="admin/upload/<?php echo $row['post_img'] ?>" alt=""/>
             </a>
             <div class="post-content">
-                <h5>Wallet</h5>
+                <h5><a href="single.php?id=<?php echo $row['post_id']; ?>"><?php echo $row['title']; ?></a></h5>
                 <span>
                     <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Wallet</a>
+                    <a href='category.php?cid=<?php echo $row['category']; ?>'><?php echo $row['category_name']; ?></a>
                 </span>
                 <span>
                     <i class="fa fa-calendar" aria-hidden="true"></i>
-                    9 June, 2023
+                    <?php echo $row['post_date']; ?>
                 </span>
-                <a class="read-more" href="single.php">read more</a>
+                <a class="read-more" href='single.php?id=<?php echo $row['post_id'] ?>'>read more</a>
             </div>
         </div>
-        <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post_1.jpg" alt=""/>
-            </a>
-            <div class="post-content">
-                <h5><a href="single.php">Notebook</a></h5>
-                <span>
-                    <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Book</a>
-                </span>
-                <span>
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    9 June, 2023
-                </span>
-                <a class="read-more" href="single.php">read more</a>
-            </div>
-        </div>
-        <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post-format.jpg" alt=""/>
-            </a>
-            <div class="post-content">
-                <h5><a href="single.php">Earphone</a></h5>
-                <span>
-                    <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Accessories</a>
-                </span>
-                <span>
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    9 June, 2023
-                </span>
-                <a class="read-more" href="single.php">read more</a>
-            </div>
-        </div>
-        <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post_1.jpg" alt=""/>
-            </a>
-            <div class="post-content">
-                <h5><a href="single.php">Pendrive</a></h5>
-                <span>
-                    <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Accessories</a>
-                </span>
-                <span>
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    9 June, 2023
-                </span>
-                <a class="read-more" href="single.php">read more</a>
-            </div>
-        </div>
-        <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/download.jfif" alt=""/>
-            </a>
-            <div class="post-content">
-                <h5><a href="single.php">A Wallet Founded</a></h5>
-                <span>
-                    <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Wallet</a>
-                </span>
-                <span>
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    9 June, 2023
-                </span>
-                <a class="read-more" href="single.php">read more</a>
-            </div>
-        </div>
+        <?php 
+            }
+        }
+        
+        ?>
+      
     </div>
     <!-- /recent posts box -->
 </div>
